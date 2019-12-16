3da4954cf8559832dc5d8af0a0bb4e86ac6ce0ad
Rogério Tavares <rogerioctf@gmail.com>
2019-12-16 08:53:32 -0300

    Create students table

---
+++ src/database/migrations/20191216114359-students.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('students', {
+            id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            nome: { type: Sequelize.STRING, allowNull: false },
+            email: { type: Sequelize.STRING, allowNull: false, unique: true },
+            idade: { type: Sequelize.INTEGER, allowNull: false },
+            peso: { type: Sequelize.DECIMAL, allowNull: false },
+            altura: { type: Sequelize.DECIMAL, allowNull: false },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        });
+    },
+
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('students');
+    },
+};
